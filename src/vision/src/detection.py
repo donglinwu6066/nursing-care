e6b2f86e3dd07e10a9782cb5b78f6f447d7dfede
donglinwu <donglin6066@gmail.com>
2021-03-25 18:33:15 +0800

    work from home

---
+++ src/vision/src/detection.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+import rospy
+from sensor_msgs.msg import LaserScan
+from geometry_msgs.msg import Twist
+import numpy as np
+
+
+class Detection(object):
+
+    def __init__(self):
+        self.ranges = None
+        self.detecrange = None
+        self.modifyrange = []
+        self.rangeAvg = 0
+        self.twist = None
+        print("initialized Detection")
+        rospy.on_shutdown(self.__shutdown_cb)
+        rospy.Subscriber("/scan", LaserScan, self.__process_detection)
+        self.pub = rospy.Publisher('/mob_plat/cmd_vel', Twist, queue_size=1)
+        rospy.spin()
+
+    def __shutdown_cb(self):
+        rospy.loginfo("Node shutdown")
+
+    def __process_detection(self, scandata):
+        self.modifyrange = []
+        self.ranges = scandata.ranges
+        # print(self.ranges)
+        self.detecrange = self.ranges[:10]+self.ranges[-10:]
+        for i in self.detecrange:
+            if i > 0.1:
+                self.modifyrange.append(i)
+        # print(self.modifyrange)
+        self.rangeAvg = sum(self.modifyrange)/len(self.modifyrange)
+        print(self.rangeAvg)
+        if self.rangeAvg > 0.5:
+            twist = Twist()
+            twist.linear.x = 0.2
+            self.pub.publish(twist)
+        else:
+            twist = Twist()
+            twist.linear.x = 0.0
+            self.pub.publish(twist)
+
+
+if __name__ == '__main__':
+    rospy.init_node('detection', anonymous=True)
+    node = Detection()
