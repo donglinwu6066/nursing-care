573aa3bd4bc64ccab3e7f6dc9f356b8c420e3a58
MARS LITE <josuet.leoro@itri.org.tw>
2021-05-14 12:33:34 +0800

    new print function

--- src/test/src/arm_control.py
+++ src/test/src/arm_control.py
@@ -20,23 +20,25 @@ from std_msgs.msg import Bool
 
 import time
 import patrol_test
-import sys, select, termios, tty
+import sys
+import select
+import termios
+import tty
 #from pose_estimate_and_pick.srv import *
 #from object_detection.srv import *
 #from apriltags_ros.msg import AprilTagDetectionArray, AprilTagDetection
 
 
+armControl = {
+    'q': (0.01, 0, 0),
+    'a': (-0.01, 0, 0),
+    'w': (0, 0.01, 0),
+    's': (0, -0.01, 0),
+    'e': (0, 0, 0.01),
+    'd': (0, 0, -0.01),
+    'x': (0, 0, 0),
+}
 
-
-armControl={
-        'q':(0.01,0,0),
-        'a':(-0.01,0,0),
-        'w':(0,0.01,0),
-        's':(0,-0.01,0),
-        'e':(0,0,0.01),
-        'd':(0,0,-0.01),
-	'x':(0,0,0),
-    }
 
 def getKey():
     tty.setraw(sys.stdin.fileno())
@@ -44,6 +46,7 @@ def getKey():
     key = sys.stdin.read(1)
     termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
     return key
+
 
 if __name__ == '__main__':
     settings = termios.tcgetattr(sys.stdin)
@@ -62,12 +65,15 @@ if __name__ == '__main__':
             if key in armControl.keys():
                 print "============ position"
                 pose_target = group.get_current_pose().pose
-                pose_target.position.x +=  armControl[key][0]
-                pose_target.position.y +=  armControl[key][1]
-                pose_target.position.z +=  armControl[key][2]
+                pose_target.position.x += armControl[key][0]
+                pose_target.position.y += armControl[key][1]
+                pose_target.position.z += armControl[key][2]
+                # pose_target.position.x += 0
+                # pose_target.position.y += 0
+                # pose_target.position.z += 0.60
                 group.set_pose_target(pose_target)
                 group.go(wait=True)
-            if(key=='h'):
+            if(key == 'h'):
                 joint_goal = group.get_current_joint_values()
                 joint_goal[0] = 0
                 joint_goal[1] = 0
@@ -75,8 +81,8 @@ if __name__ == '__main__':
                 joint_goal[3] = -pi/2
                 joint_goal[4] = pi/2
                 joint_goal[5] = 0
-                group.go(joint_goal, wait=True) 
-            if(key=='g'):
+                group.go(joint_goal, wait=True)
+            if(key == 'g'):
                 joint_goal = group.get_current_joint_values()
                 joint_goal[0] = -1.5638910532
                 joint_goal[1] = 0.0138541124761
@@ -84,8 +90,8 @@ if __name__ == '__main__':
                 joint_goal[3] = -2.22698187828
                 joint_goal[4] = 1.54945480824
                 joint_goal[5] = 0.00317068165168
-                group.go(joint_goal, wait=True)  
-            if(key=='o'):
+                group.go(joint_goal, wait=True)
+            if(key == 'o'):
                 joint_goal = group.get_current_joint_values()
                 joint_goal[0] = -1.5707963705062866
                 joint_goal[1] = -0.7330362200737
@@ -93,8 +99,8 @@ if __name__ == '__main__':
                 joint_goal[3] = -0.1745329201221466
                 joint_goal[4] = 1.5707963705062866
                 joint_goal[5] = 0.0
-                group.go(joint_goal, wait=True)    
-            if(key=='i'):
+                group.go(joint_goal, wait=True)
+            if(key == 'i'):
                 joint_goal = group.get_current_joint_values()
                 joint_goal[0] = 0.0003732585464604199
                 joint_goal[1] = -0.7324762940406799
@@ -102,73 +108,74 @@ if __name__ == '__main__':
                 joint_goal[3] = -0.17437778413295746
                 joint_goal[4] = 1.5701175928115845
                 joint_goal[5] = -0.00033936958061531186
-                group.go(joint_goal, wait=True) 
-            if(key=='l'):
+                group.go(joint_goal, wait=True)
+            if(key == 'l'):
                 joint_goal = group.get_current_joint_values()
-                joint_goal = [1.6443593502044678, 1.69889235496521, -1.8340119123458862, 0.1071632131934166, 1.4464706182479858, -0.0030737188644707203]
-                group.go(joint_goal, wait=True) 
-            if(key=='r'):
+                joint_goal = [1.6443593502044678, 1.69889235496521, -1.8340119123458862,
+                              0.1071632131934166, 1.4464706182479858, -0.0030737188644707203]
+                group.go(joint_goal, wait=True)
+            if(key == 'r'):
                 pub.publish(True)
-            if(key=='f'):
+            if(key == 'f'):
                 pub.publish(False)
             if(key == 'z'):
-                joint_goal = group.get_current_joint_values()	
+                joint_goal = group.get_current_joint_values()
                 joint_goal[0] += pi/12
-                group.go(joint_goal, wait=True) 
+                group.go(joint_goal, wait=True)
             if(key == 'x'):
-                joint_goal = group.get_current_joint_values()	
+                joint_goal = group.get_current_joint_values()
                 joint_goal[0] -= pi/12
-                group.go(joint_goal, wait=True) 
+                group.go(joint_goal, wait=True)
             if(key == 'c'):
-                joint_goal = group.get_current_joint_values()	
+                joint_goal = group.get_current_joint_values()
                 joint_goal[1] += pi/12
-                group.go(joint_goal, wait=True) 
+                group.go(joint_goal, wait=True)
             if(key == 'v'):
-                joint_goal = group.get_current_joint_values()	
+                joint_goal = group.get_current_joint_values()
                 joint_goal[1] -= pi/12
-                group.go(joint_goal, wait=True) 
+                group.go(joint_goal, wait=True)
             if(key == 'b'):
-                joint_goal = group.get_current_joint_values()	
+                joint_goal = group.get_current_joint_values()
                 joint_goal[2] += pi/12
-                group.go(joint_goal, wait=True) 
+                group.go(joint_goal, wait=True)
             if(key == 'n'):
-                joint_goal = group.get_current_joint_values()	
+                joint_goal = group.get_current_joint_values()
                 joint_goal[2] -= pi/12
-                group.go(joint_goal, wait=True) 
+                group.go(joint_goal, wait=True)
             if(key == 'm'):
-                joint_goal = group.get_current_joint_values()	
+                joint_goal = group.get_current_joint_values()
                 joint_goal[3] += pi/12
-                group.go(joint_goal, wait=True) 
+                group.go(joint_goal, wait=True)
             if(key == ','):
-                joint_goal = group.get_current_joint_values()	
+                joint_goal = group.get_current_joint_values()
                 joint_goal[3] -= pi/12
-                group.go(joint_goal, wait=True) 
+                group.go(joint_goal, wait=True)
             if(key == '.'):
-                joint_goal = group.get_current_joint_values()	
+                joint_goal = group.get_current_joint_values()
                 joint_goal[4] += pi/24
-                group.go(joint_goal, wait=True) 
+                group.go(joint_goal, wait=True)
             if(key == '/'):
-                joint_goal = group.get_current_joint_values()	
+                joint_goal = group.get_current_joint_values()
                 joint_goal[4] -= pi/24
                 group.go(joint_goal, wait=True)
             if(key == 'j'):
-                joint_goal = group.get_current_joint_values()	
+                joint_goal = group.get_current_joint_values()
                 joint_goal[5] += pi/12
-                group.go(joint_goal, wait=True) 
+                group.go(joint_goal, wait=True)
             if(key == 'k'):
-                joint_goal = group.get_current_joint_values()	
+                joint_goal = group.get_current_joint_values()
                 joint_goal[5] -= pi/12
-                group.go(joint_goal, wait=True)  
+                group.go(joint_goal, wait=True)
             if(key == 'y'):
                 joint_goal = group.get_current_joint_values()
-		joint_goal[0] = 1.66030991
+                joint_goal[0] = 1.66030991
                 joint_goal[1] = 1.68294799
                 joint_goal[2] = -1.75615036
                 joint_goal[3] = 0.05834247
                 joint_goal[4] = -0.016470089
                 joint_goal[5] = -0.0313480533
-                group.go(joint_goal, wait=True) 	
-            if(key=='u'):
+                group.go(joint_goal, wait=True)
+            if(key == 'u'):
                 joint_goal = group.get_current_joint_values()
                 joint_goal[0] = 1.5700684
                 joint_goal[1] = -0.003091824
@@ -177,7 +184,7 @@ if __name__ == '__main__':
                 joint_goal[4] = 0.005206889
                 joint_goal[5] = 0.0471839
                 group.go(joint_goal, wait=True)
-            if(key=='p'):
+            if(key == 'p'):
                 joint_goal = group.get_current_joint_values()
                 joint_goal[0] = 1.760131716728
                 joint_goal[1] = 1.6801549
@@ -186,22 +193,46 @@ if __name__ == '__main__':
                 joint_goal[4] = -0.116103179
                 joint_goal[5] = 0.77145493
                 group.go(joint_goal, wait=True)
-		raw_input("0")
-                pub.publish(True)
-		raw_input("1")
-                joint_goal = group.get_current_joint_values()	
-                joint_goal[5] += pi/4
-                group.go(joint_goal, wait=True) 
-		raw_input("2")
+                # raw_input("0")
+                # pub.publish(True)
+                # raw_input("1")
+                #joint_goal = group.get_current_joint_values()
+                #joint_goal[5] += pi/4
+                #group.go(joint_goal, wait=True)
+                # raw_input("2")
+                #pose_target = group.get_current_pose().pose
+                #pose_target.position.x +=  0.06
+                # group.set_pose_target(pose_target)
+                # group.go(wait=True)
+            if(key == '['):
                 pose_target = group.get_current_pose().pose
-                pose_target.position.x +=  0.06
+                pose_target.position.x = 0.47
+                pose_target.position.y = -0.01337232
+                pose_target.position.z = 0.82400006
+                group.set_pose_target(pose_target)
+                group.go(wait=True)
+            if(key == ']'):
+                pose_target = group.get_current_pose().pose
+		print("position:")
+		print(pose_target.position.x)
+		print(pose_target.position.y)
+		print(pose_target.position.z)
+		print("quaternion:")
+		print(pose_target.orientation.x)
+		print(pose_target.orientation.y)
+		print(pose_target.orientation.z)
+		print(pose_target.orientation.w)
+            if(key == '0'):
+                pose_target = group.get_current_pose().pose
+		pose_target.orientation.x = 1
+		pose_target.orientation.y = 0
+		pose_target.orientation.z = 0
+		pose_target.orientation.w = 0
                 group.set_pose_target(pose_target)
                 group.go(wait=True)
 
-			
-	    if(key == '\x03'):
+            if(key == '\x03'):
                 break
-
 
     except Exception as e:
         print(e)
@@ -209,4 +240,4 @@ if __name__ == '__main__':
     finally:
         termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
         print "============ Bye"
-    #rospy.spin()
+    # rospy.spin()
