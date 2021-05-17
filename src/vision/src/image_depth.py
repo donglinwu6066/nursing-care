29bf9591ade92630ac7120dd0f9f5ca985a5a7f1
donglinwu6066 <donglin6066@gmail.com>
2021-05-17 19:13:01 +0800

    add service

--- src/vision/src/image_depth.py
+++ src/vision/src/image_depth.py
@@ -11,27 +11,39 @@ from darknet_ros_msgs.msg import BoundingBoxes, BoundingBox
 from geometry_msgs.msg import Point
 import tf
 from vision.msg import Floats
+from vision.srv import Rotation
 
 DEPTH_IMAGE_TOPIC = "/camera/aligned_depth_to_color/image_raw"
 CAMERA_INFO_TOPIC = "/camera/color/camera_info"
 TARGET_CLASS = 'doorknob'
-# CAMERA_RESOLUTION = (480, 640)
+CAMERA_RESOLUTION = (480, 640)
 
 
 class Image_depth(object):
 
     def __init__(self):
+
+        rospy.init_node('image_depth', anonymous=True)
+        # distance from objects to camera
         self.pub_doorknob = rospy.Publisher(
             "nursing/doorknob_depth", data_class=Floats, queue_size=5)
+        # rotate degree to parallel with wall
         # self.pub_rotation = rospy.Publisher(
-        # "nursing/rotation_radian", data_class=np.float32)
-        rospy.init_node('image_depth', anonymous=True)
+        #     "nursing/rotation_radian", data_class=Floats, queue_size=5)
+        self.srv_rotation = rospy.Service(
+            'rotation', service_class=Rotation, handler=self.get_rotation)
 
+        # publish frequency
         self.rate = rospy.Rate(2)
+
         # CVand ROSImage brige
         self.cvbridge = CvBridge()
-        self.intrinsic_matrix = None
 
+        # depth array
+        self.depth_array = None
+
+        # set intrinsic_matrix for camera
+        self.intrinsic_matrix = None
         self.get_intrinsic_matrix()
 
     def get_intrinsic_matrix(self):
@@ -72,40 +84,52 @@ class Image_depth(object):
                     print("depth of box center: {:.2f}mm".format(
                         self.depth_array[ymid, xmid]))
                     p = Point(xmid, ymid, self.depth_array[ymid, xmid])
+                #  if depth_array is None, find its neighbors:
                 else:
                     for i in range(-2, 2):
                         for j in range(-2, 2):
                             # originally, self.depth_array[ymid+i, xmid+j]
                             if self.depth_array[ymid+i, xmid+j] is not None:
-                                # print("depth of box center: {:.2f}mm".format(self.depth_array[ymid+i, xmid+j]))
                                 p = Point(ymid+i, xmid+j,
                                           self.depth_array[ymid+i, xmid+j])
 
+                                # double loop breaker
                                 i = j = 2
-                                break
+
+        # get horizontal_y and vertical_z from 2D
         vector_cam = self.pixel2cam(p)
-        # vector_cam = np.array([ymid, xmid, self.depth_array[ymid, xmid]])
-        temp = vector_cam[0]
-        temp1 = vector_cam[1]
+
+        # swap dimension to fit our camera orientation,
+        horizontal_y = vector_cam[0]
+        vertical_z = vector_cam[1]
         vector_cam[0] = self.depth_array[ymid, xmid]
-        vector_cam[1] = temp
-        vector_cam[2] = temp1
-        # vector_cam[0] = self.depth_array[ymid, xmid]
-        # vector_cam[2] =
-        # self.pub_doorknob.publish(vector_cam)
-        # rospy.loginfo(vector_cam)
-        # print(vector_cam)
+        vector_cam[1] = horizontal_y
+        vector_cam[2] = vertical_z
+
+        # publish [x, y, z, 1]
         self.pub_doorknob.publish(vector_cam)
 
-    def get_point():
-        print("UNFINISHED")
+    def get_rotation(self, req):
 
-    def get_rotation(self):
-        print("UNFINISHED")
+        print("UNDER CONSTRUCTION")
+        # get left and right point on image
+        left = self.depth_array[CAMERA_RESOLUTION[0]/2, CAMERA_RESOLUTION[0]/3]
+        right = self.depth_array[CAMERA_RESOLUTION[0] /
+                                 2, CAMERA_RESOLUTION[0]*2/3]
+
+        if (left - right) > 0.1:
+            rotate = "counterclockwise"
+        elif (left - right) > 0.1:
+            rotate = "clockwise"
+        else:
+            rotate = "balanced"
+
+        return vision.srv.RotationResponse(rotate)
 
     def pixel2cam(self, p):
         # Vector in pixel coordinate s[u, v, 1]*T
         vector_pixel = np.array([p.x, p.y, 1.0]).transpose() * p.z
+
         # Vector in camera coordinate
         vector_cam = np.dot(np.linalg.inv(self.intrinsic_matrix), vector_pixel)
         vector_cam = np.append(vector_cam, [1], 0).transpose()
@@ -126,7 +150,6 @@ class Image_depth(object):
         vector_cam = np.dot(np.linalg.inv(self.intrinsic_matrix), vector_pixel)
         vector_cam = np.append(vector_cam, [1], 0).transpose()
         # rospy.loginfo(vector_cam)
-        # self.pub_doorknob.publish(vector_cam)
 
         # rospy.loginfo(self.depth_array)
         # self.pub_doorknob.publish(self.depth_array)
@@ -135,8 +158,11 @@ class Image_depth(object):
 
     def image_depth(self):
         while not rospy.is_shutdown():
-            sub_tf = rospy.Subscriber(
-                "nursing/mid_depth", data_class=Floats, callback=self.get_depth_array())
+            self.get_depth_array()
+
+            self.publish_rotation()
+            # sub_tf = rospy.Subscriber(
+            # "nursing/mid_depth", data_class=Floats, callback=self.get_depth_array())
             sub_bbox = rospy.Subscriber(
                 "darknet_ros/bounding_boxes", BoundingBoxes, callback=self.get_bbox, queue_size=1)
 
